67edd65d56c24a008ecdba4a0cdd011a9a9cbc9f
Ryan Bell <ryan@piing.net>
2019-10-11 13:48:33 -0700

    Health check

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/server/health')
+def server_health():
+  return jsonify(success=True)
