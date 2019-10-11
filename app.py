bdc4f7e94d50be11044415e209fe21ada2ffaa00
Ryan Bell <ryan@piing.net>
2019-10-11 15:30:19 -0700

    Added weight/sensitivity endpoint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+@app.route('/weight/sensitivity', methods=['POST'])
+def weight_sensitivity():
+	# content = request.get_json(silent=True)
+	return jsonify({"success": True})
