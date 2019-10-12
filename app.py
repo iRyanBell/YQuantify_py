03c36354c38fb8ff2a99df82e1349951d6ade15f
Ryan Bell <ryan@piing.net>
2019-10-11 23:16:07 -0700

    Debug: route return value

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
-import weight_sensitivity
+# import weight_sensitivity
 
 app = Flask(__name__)
 
 
 @app.route('/weight/sensitivity', methods=['POST'])
 def weight_sensitivity_analysis():
-    key = request.form['key']
-    return jsonify(weight_sensitivity.perform_analysis(key=key))
+    # key = request.form['key']
+    return jsonify({'success': True})
