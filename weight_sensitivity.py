62f2e0663b9cb15a0aeb3611449ed4af853c76cc
Ryan Bell <ryan@piing.net>
2019-10-12 16:07:33 -0700

    Renamed to request_document (was request_content)

--- weight_sensitivity.py
+++ weight_sensitivity.py
@@ -13,7 +13,7 @@ warnings.filterwarnings("ignore")
 ##############################################
 
 
-def request_content(url):
+def request_document(url):
   # Returns a utf-8 document from a url.
   with urlopen(url) as f:
     return f.read().decode('utf-8')
@@ -26,7 +26,7 @@ def request_content(url):
 def perform_analysis(key='', sensitivity_ratio=0.3):
   # Request CSV from SQL table
   # Return error if the server responds with unexpected JSON data.
-  res = request_content('https://www.yquantify.com/csv/daily.csv?key=' + key)
+  res = request_document('https://www.yquantify.com/csv/daily.csv?key=' + key)
   if res[0] == '{':
     try:
       resObj = json.loads(res)
