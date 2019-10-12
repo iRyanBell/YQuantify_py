18c9a37bba3437949f335766f0cb5ed8cdb3a19c
Ryan Bell <ryan@piing.net>
2019-10-12 15:39:49 -0700

    Check for error

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route('/weight/sensitivity', methods=['POST'])
 def weight_sensitivity_analysis():
-    form_data = request.get_json()
-    key = form_data['key']
-    results = weight_sensitivity.perform_analysis(key=key)
-    return jsonify({'results': results})
+  form_data = request.get_json()
+  key = form_data['key']
+  res = weight_sensitivity.perform_analysis(key=key)
+  if 'error' in res:
+    return jsonify({'error': res['error']})
+  return jsonify({'results': res['results']})
--- weight_sensitivity.py
+++ weight_sensitivity.py
@@ -13,84 +13,84 @@ SENSITIVITY_RATIO = 0.3
 
 
 def perform_analysis(key=''):
-    # Read CSV from SQL table
-    try:
-        df = pd.read_csv('https://www.yquantify.com/csv/daily.csv?key=' + key)
-    except:
-        return {'error': 'csv'}
+  # Read CSV from SQL table
+  try:
+    df = pd.read_csv('https://www.yquantify.com/csv/daily.csv?key=' + key)
+  except:
+    return {'error': 'csv'}
 
-    ##############################################
-    # DATA EXPLORATION
-    ##############################################
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
+  ##############################################
+  # DATA EXPLORATION
+  ##############################################
+  df['date'] = pd.to_datetime(df['date'])
+  df.set_index('date', inplace=True)
 
-    columns = ['sleep', 'exercise', 'calories', 'weight']
-    df = df.copy()
-    df = df[columns]
+  columns = ['sleep', 'exercise', 'calories', 'weight']
+  df = df.copy()
+  df = df[columns]
 
-    ##############################################
-    # Data manupulation for NAN value
-    ##############################################
-    nan_columns = df.isnull().any()
-    nan_true = [i for i, col_i in enumerate(nan_columns) if col_i == True]
+  ##############################################
+  # Data manupulation for NAN value
+  ##############################################
+  nan_columns = df.isnull().any()
+  nan_true = [i for i, col_i in enumerate(nan_columns) if col_i == True]
 
-    for nan_i in nan_true:
-        column_name = nan_columns.index[nan_i]
-        s = pd.Series(df[column_name])
-        df[column_name] = s.interpolate(method='nearest')
+  for nan_i in nan_true:
+    column_name = nan_columns.index[nan_i]
+    s = pd.Series(df[column_name])
+    df[column_name] = s.interpolate(method='nearest')
 
-    ##############################################
-    # ARIMA
-    # (Auto-Regressive Integrated Moving Average)
-    ##############################################
+  ##############################################
+  # ARIMA
+  # (Auto-Regressive Integrated Moving Average)
+  ##############################################
 
-    train = df[:-1]
+  train = df[:-1]
 
-    # Make final predictions
-    model = VAR(endog=train)
-    model_fit = model.fit()
-    yhat = model_fit.forecast(model_fit.y, steps=2)
+  # Make final predictions
+  model = VAR(endog=train)
+  model_fit = model.fit()
+  yhat = model_fit.forecast(model_fit.y, steps=2)
 
-    ##############################################
-    # Sensitivity
-    ##############################################
+  ##############################################
+  # Sensitivity
+  ##############################################
 
-    def sensitivity(df, col_name, ratio, percentage=0.9):
-        df_sen[col_name].iloc[-2] = df_sen[col_name].iloc[-2] * ratio
+  def sensitivity(df, col_name, ratio, percentage=0.9):
+    df_sen[col_name].iloc[-2] = df_sen[col_name].iloc[-2] * ratio
 
-        train = df_sen[:-1]
+    train = df_sen[:-1]
 
-        model_sen = VAR(endog=train)
-        model_sen_fit = model_sen.fit()
+    model_sen = VAR(endog=train)
+    model_sen_fit = model_sen.fit()
 
-        # Make prediction on validation
-        yhat_sen_cal = model_sen_fit.forecast(model_sen_fit.y, steps=2)
-        return yhat_sen_cal[:, 3][-1]
+    # Make prediction on validation
+    yhat_sen_cal = model_sen_fit.forecast(model_sen_fit.y, steps=2)
+    return yhat_sen_cal[:, 3][-1]
 
-    col_names_sensitivity = {
-        'sleep': 1+SENSITIVITY_RATIO,
-        'exercise': 1+SENSITIVITY_RATIO,
-        'calories': 1-SENSITIVITY_RATIO}
+  col_names_sensitivity = {
+      'sleep': 1+SENSITIVITY_RATIO,
+      'exercise': 1+SENSITIVITY_RATIO,
+      'calories': 1-SENSITIVITY_RATIO}
 
-    weight_pred = []
-    weight_diff = []
-    for col_i, direction in col_names_sensitivity.items():
-        df_sen = df.copy()
-        res = sensitivity(df_sen, col_i, direction, percentage=0.1)
-        weight_pred.append(res)
-        weight_diff.append(yhat[1, 3] - res)
+  weight_pred = []
+  weight_diff = []
+  for col_i, direction in col_names_sensitivity.items():
+    df_sen = df.copy()
+    res = sensitivity(df_sen, col_i, direction, percentage=0.1)
+    weight_pred.append(res)
+    weight_diff.append(yhat[1, 3] - res)
 
-    ##############################################
-    # Final result
-    ##############################################
-    df_sen = pd.DataFrame()
-    df_sen['attr'] = col_names_sensitivity.keys()
-    df_sen['weight_pred'] = weight_pred
-    df_sen['sensitivity'] = np.array(weight_diff) * -1
+  ##############################################
+  # Final result
+  ##############################################
+  df_sen = pd.DataFrame()
+  df_sen['attr'] = col_names_sensitivity.keys()
+  df_sen['weight_pred'] = weight_pred
+  df_sen['sensitivity'] = np.array(weight_diff) * -1
 
-    results = {}
-    for idx, attr in enumerate(df_sen['attr']):
-        results[attr] = df_sen['sensitivity'][idx]
+  results = {}
+  for idx, attr in enumerate(df_sen['attr']):
+    results[attr] = df_sen['sensitivity'][idx]
 
-    return results
+  return {'results': results}
