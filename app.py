56793163ea853cb2f8b39c05f9afe33eec2a396d
Ryan Bell <ryan@piing.net>
2019-10-11 20:47:57 -0700

    Shortened name

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
-import weight_sensitivity_analysis
+import weight_sensitivity
 
 app = Flask(__name__)
 
 
 @app.route('/weight/sensitivity', methods=['POST'])
-def weight_sensitivity():
-    key = request.form.key
-    return jsonify(weight_sensitivity_analysis.perform_analysis(key=key))
+def weight_sensitivity_analysis():
+    key = request.form['key']
+    return jsonify(weight_sensitivity.perform_analysis(key=key))
